aab75b1e12cb38364d699c8df41c3ee5be92e6cf
Denis Kenzior <denkenz@gmail.com>
2019-12-06 09:54:28 -0600

    dbus: Fix warning on asan/usan/ubsan builds
      CC       ell/dbus-filter.lo
    ell/dbus-filter.c: In function ‘_dbus_filter_add_rule’:
    ell/dbus-filter.c:318:19: error: ‘condition’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
      318 |   condition->type = L_DBUS_MATCH_NONE;
          |   ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~
    ell/dbus-filter.c:322:12: error: ‘node’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
      322 |   node_ptr = &node->match.children;
          |   ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- ell/dbus-filter.c
+++ ell/dbus-filter.c
@@ -240,11 +240,12 @@ unsigned int _dbus_filter_add_rule(struct _dbus_filter *filter,
 				void *user_data)
 {
 	struct filter_node **node_ptr = &filter->root;
-	struct filter_node *node;
+	struct filter_node *uninitialized_var(node);
 	struct filter_node *parent = filter->root;
 	bool remote_rule = false;
 	struct _dbus_filter_condition sorted[rule_len];
-	struct _dbus_filter_condition *unused, *condition;
+	struct _dbus_filter_condition *unused;
+	struct _dbus_filter_condition *uninitialized_var(condition);
 	struct _dbus_filter_condition *end = sorted + rule_len;
 
 	memcpy(sorted, rule, sizeof(sorted));
