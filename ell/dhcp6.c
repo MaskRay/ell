8987b3d86dcc4d1539b227a79b6be37c2643c196
Tim Kourt <tim.a.kourt@linux.intel.com>
2020-07-02 15:17:08 -0500

    dhcp6: Add client state tracking enum

--- ell/dhcp6.c
+++ ell/dhcp6.c
@@ -36,7 +36,19 @@
 #define CLIENT_DEBUG(fmt, args...)					\
 	l_util_debug(client->debug_handler, client->debug_data,		\
 			"%s:%i " fmt, __func__, __LINE__, ## args)
+
+enum dhcp6_state {
+	DHCP6_STATE_INIT,
+	DHCP6_STATE_SOLICITING,
+	DHCP6_STATE_REQUESTING_INFORMATION,
+	DHCP6_STATE_REQUESTING,
+	DHCP6_STATE_RENEWING,
+	DHCP6_STATE_RELEASING,
+};
+
 struct l_dhcp6_client {
+	enum dhcp6_state state;
+
 	uint32_t ifindex;
 
 	uint8_t addr[6];
@@ -53,6 +65,8 @@ LIB_EXPORT struct l_dhcp6_client *l_dhcp6_client_new(uint32_t ifindex)
 	struct l_dhcp6_client *client;
 
 	client = l_new(struct l_dhcp6_client, 1);
+
+	client->state = DHCP6_STATE_INIT;
 	client->ifindex = ifindex;
 
 	return client;
@@ -113,6 +127,9 @@ LIB_EXPORT bool l_dhcp6_client_set_debug(struct l_dhcp6_client *client,
 LIB_EXPORT bool l_dhcp6_client_start(struct l_dhcp6_client *client)
 {
 	if (unlikely(!client))
+		return false;
+
+	if (unlikely(client->state != DHCP6_STATE_INIT))
 		return false;
 
 	if (!client->addr_len) {
