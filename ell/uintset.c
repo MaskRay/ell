MaskRay/ell

commit 8e8568614502415ca35860647c8b514b459fa85b

uintset: Add l_uintset_clone

ell/uintset.c
@@ -471,6 +471,26 @@ LIB_EXPORT void l_uintset_foreach(struct l_uintset *set,
 }
 
 /**
+ * l_uintset_clone:
+ * @original: The set of numbers
+ * @set_b: The set of numbers
+ *
+ * Returns: A newly allocated l_uintset object containing a copy of @original
+ **/
+LIB_EXPORT struct l_uintset *l_uintset_clone(const struct l_uintset *original)
+{
+	struct l_uintset *clone;
+
+	if (unlikely(!original))
+		return NULL;
+
+	clone = l_uintset_new_from_range(original->min, original->max);
+	memcpy(clone->bits, original->bits, original->size); 
+
+	return clone;
+}
+
+/**
  * l_uintset_intersect:
  * @set_a: The set of numbers
  * @set_b: The set of numbers

ell/ell.sym
@@ -486,6 +486,7 @@ global:
 	l_uintset_find_max;
 	l_uintset_find_min;
 	l_uintset_foreach;
+	l_uintset_clone;
 	l_uintset_intersect;
 	l_uintset_isempty;
 	/* uuid */

ell/uintset.h
@@ -55,6 +55,7 @@ uint32_t l_uintset_find_unused(struct l_uintset *set, uint32_t start);
 void l_uintset_foreach(struct l_uintset *set,
 			l_uintset_foreach_func_t function, void *user_data);
 
+struct l_uintset *l_uintset_clone(const struct l_uintset *original);
 struct l_uintset *l_uintset_intersect(const struct l_uintset *set_a,
 						const struct l_uintset *set_b);
 bool l_uintset_isempty(const struct l_uintset *set);

commit f6129fa480ef5636069c7da348f92178b70e939e

uintset: Fix number of bytes copied
uintset size is the number of bits in the set, not bytes allocated the
bitmap.  Fix that.

Fixes: 8e8568614502 ("uintset: Add l_uintset_clone")

ell/uintset.c
@@ -480,12 +480,15 @@ LIB_EXPORT void l_uintset_foreach(const struct l_uintset *set,
 LIB_EXPORT struct l_uintset *l_uintset_clone(const struct l_uintset *original)
 {
 	struct l_uintset *clone;
+	size_t bitmap_size;
 
 	if (unlikely(!original))
 		return NULL;
 
+	bitmap_size = (original->size + BITS_PER_LONG - 1) / BITS_PER_LONG;
+
 	clone = l_uintset_new_from_range(original->min, original->max);
-	memcpy(clone->bits, original->bits, original->size); 
+	memcpy(clone->bits, original->bits, bitmap_size);
 
 	return clone;
 }