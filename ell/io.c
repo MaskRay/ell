42bf5ad20b45a0435ab489b73960c01f66014320
Inga Stotland <inga.stotland@intel.com>
2020-10-17 23:06:44 -0700

    io: Always call read handler on EPOLLIN
    When detecting EPOLLIN, always invoke read handler first.
    This ensures that even when error conditions (i.e., EPOLLHUP
    and/or EPOLLERR) are encountered, any residual data is still
    delivered to the client.

--- ell/io.c
+++ ell/io.c
@@ -111,14 +111,6 @@ static void io_callback(int fd, uint32_t events, void *user_data)
 {
 	struct l_io *io = user_data;
 
-	if (unlikely(events & (EPOLLERR | EPOLLHUP))) {
-		l_util_debug(io->debug_handler, io->debug_data,
-						"disconnect event <%p>", io);
-		watch_remove(io->fd, !io->close_on_destroy);
-		io_closed(io);
-		return;
-	}
-
 	if ((events & EPOLLIN) && io->read_handler) {
 		l_util_debug(io->debug_handler, io->debug_data,
 						"read event <%p>", io);
@@ -140,6 +132,14 @@ static void io_callback(int fd, uint32_t events, void *user_data)
 				return;
 			}
 		}
+	}
+
+	if (unlikely(events & (EPOLLERR | EPOLLHUP))) {
+		l_util_debug(io->debug_handler, io->debug_data,
+						"disconnect event <%p>", io);
+		watch_remove(io->fd, !io->close_on_destroy);
+		io_closed(io);
+		return;
 	}
 
 	if ((events & EPOLLOUT) && io->write_handler) {
