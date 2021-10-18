MaskRay/ell

commit 42e1357bb99a73262087c60a055e20a19cb42273

dhcp: Add l_dhcp_lease_get_prefix_length
Simple utility to obtain the prefix length.  This is typically more
useful than the netmask in string form since most of the utilities
associated with l_rtnl_* are using the prefix length.

ell/dhcp-lease.c
@@ -223,6 +223,25 @@ LIB_EXPORT char *l_dhcp_lease_get_netmask(const struct l_dhcp_lease *lease)
 	return get_ip(lease->subnet_mask);
 }
 
+LIB_EXPORT uint32_t l_dhcp_lease_get_prefix_length(
+					const struct l_dhcp_lease *lease)
+{
+	int pl;
+
+	if (unlikely(!lease))
+		return 0;
+
+	if (!lease->subnet_mask)
+		return 0;
+
+	pl = __builtin_popcount(lease->subnet_mask);
+
+	if (__builtin_ctz(lease->subnet_mask) != 32 - pl)
+		return 0;
+
+	return pl;
+}
+
 LIB_EXPORT char *l_dhcp_lease_get_broadcast(const struct l_dhcp_lease *lease)
 {
 	if (unlikely(!lease))

ell/dhcp.h
@@ -108,6 +108,7 @@ bool l_dhcp_client_set_debug(struct l_dhcp_client *client,
 char *l_dhcp_lease_get_address(const struct l_dhcp_lease *lease);
 char *l_dhcp_lease_get_gateway(const struct l_dhcp_lease *lease);
 char *l_dhcp_lease_get_netmask(const struct l_dhcp_lease *lease);
+uint32_t l_dhcp_lease_get_prefix_length(const struct l_dhcp_lease *lease);
 char *l_dhcp_lease_get_broadcast(const struct l_dhcp_lease *lease);
 char *l_dhcp_lease_get_server_id(const struct l_dhcp_lease *lease);
 const uint8_t *l_dhcp_lease_get_server_mac(const struct l_dhcp_lease *lease);

ell/ell.sym
@@ -228,6 +228,7 @@ global:
 	l_dhcp_lease_get_gateway;
 	l_dhcp_lease_get_mac;
 	l_dhcp_lease_get_netmask;
+	l_dhcp_lease_get_prefix_length;
 	l_dhcp_lease_get_broadcast;
 	l_dhcp_lease_get_server_id;
 	l_dhcp_lease_get_server_mac;

commit 1265a1af3e184eedb75a52cd0ee630ed46c6f622

dhcp-lease: Fix prefix length sanity check
Since subnet_mask is in network order, convert it prior to using ctz.

Fixes: 42e1357bb99a ("dhcp: Add l_dhcp_lease_get_prefix_length")

ell/dhcp-lease.c
@@ -263,7 +263,7 @@ LIB_EXPORT uint32_t l_dhcp_lease_get_prefix_length(
 
 	pl = __builtin_popcount(lease->subnet_mask);
 
-	if (__builtin_ctz(lease->subnet_mask) != 32 - pl)
+	if (__builtin_ctz(L_BE32_TO_CPU(lease->subnet_mask)) != 32 - pl)
 		return 0;
 
 	return pl;